4a6073ece5f8013f5559e47cbc120f7a34be0d8f
honey193 <honeyben.shah@zopsmart.com>
2023-02-20 11:56:38 +0530

    Initial Commit

---
+++ PackageExample/PackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackageExampleApp.swift
+//  PackageExample
+//
+//  Created by Honey Shah on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
